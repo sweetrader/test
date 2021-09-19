2d58bbc5db3d6abcbbb265492d50e4a89f28ece8
taewoo <sweetrader2@gmail.com>
2021-09-19 15:00:31 +0900

    first commit

---
+++ src/test/java/com/example/taewootest/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taewootest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
